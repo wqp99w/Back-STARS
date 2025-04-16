17d214b96ede05eaac77ec980fba0f8726f3f789
ha-yun <deu05230@naver.com>
2025-04-16 14:33:57 +0900

    Feat:관광지 및 지역 테이블 생성

---
+++ place-service/src/main/java/com/example/placeservice/entity/Area.java
@@ -0,0 +1,27 @@
+package com.example.placeservice.entity;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+public class Area {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "area_id")
+    private Long area_id;
+
+    @Column(name = "name", length = 50, nullable = false)
+    private String name;  // 지역명
+
+    @Column(name = "category", length = 20, nullable = false)
+    private String category;  // 구분
+
+    @Column(name = "x", precision = 10, scale = 6, nullable = false)
+    private BigDecimal x; // 위도
+
+    @Column(name = "y", precision = 10, scale = 6, nullable = false)
+    private BigDecimal y; // 경도
+
+
+}
