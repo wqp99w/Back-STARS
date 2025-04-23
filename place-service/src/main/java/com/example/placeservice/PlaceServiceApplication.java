d5821aeeda065df913d91f899931ad3da2a217e2
YunHwa0905 <yoho030905@gmail.com>
2025-04-23 14:36:43 +0900

    Feat: Build 관련 수정

--- place-service/src/main/java/com/example/placeservice/PlaceServiceApplication.java
+++ place-service/src/main/java/com/example/placeservice/PlaceServiceApplication.java
@@ -1,5 +1,7 @@
 package com.example.placeservice;
 
+import com.example.placeservice.service.CafeService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -17,5 +19,13 @@ public class PlaceServiceApplication {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public CommandLineRunner initData(CafeService cafeService) {
+        return args -> {
+            // 애플리케이션 시작 시 모든 장소의 주변 카페 데이터 처리
+            cafeService.processAllAreas();
+        };
     }
 }
